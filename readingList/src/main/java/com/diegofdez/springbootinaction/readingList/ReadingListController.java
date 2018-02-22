26b867997ce0bfbaf87a651b36d1699c51499ad1
Diego FL <diegofdez@gmail.com>
2018-02-22 10:00:29 -0600

    Changes to use Spring settings. Logging, Data connection and constants in application properties

--- readingList/src/main/java/com/diegofdez/springbootinaction/readingList/ReadingListController.java
+++ readingList/src/main/java/com/diegofdez/springbootinaction/readingList/ReadingListController.java
@@ -3,20 +3,27 @@ package com.diegofdez.springbootinaction.readingList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon") // All amazon.* properties from application.properties will be sent to setters in this class
 public class ReadingListController {
+	private String associateId;
 	private ReadingListRepository readingListRepository;
 	
 	@Autowired
 	public ReadingListController(ReadingListRepository readingListRepository) {
 		this.readingListRepository = readingListRepository;
+	}
+	
+	// The value is set because of ConfigurationProperties annotation
+	public void setAssociateId(String associateId) {
+		this.associateId = associateId;
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
@@ -25,6 +32,7 @@ public class ReadingListController {
 		
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
+			model.addAttribute("amazonId", associateId);
 		}
 		
 		return "readingList";
--- readingList/src/main/resources/data.sql
+++ readingList/src/main/resources/data.sql
@@ -1,1 +1,2 @@
 insert into Reader (username, password, fullname) values ('diegofdez', 'password', 'Diego Fernandez');
+insert into Reader (username, password, fullname) values ('dajaziel', '1234', 'Daly Enciso');
